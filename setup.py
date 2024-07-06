9b0e6a9b8074c5f002ef5962ccd0c5910c1f1da0
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2024-07-06 16:13:32 +0500

    modify requirements.txt

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='github_downloader',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'github_downloader=github_downloader:main',
+        ],
+    },
+    author='Sufiyaan Usmani',
+    author_email='usmanisufiyaan@gmail.com',
+    description='A simple tool to download GitHub repositories or folders.',
+    url='https://github.com/yourusername/github_downloader',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
