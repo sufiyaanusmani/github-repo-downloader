e0f5c19f23d3905cd26f3d25be25115a83126510
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2024-07-06 16:16:22 +0500

    modify directory structure

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     author='Sufiyaan Usmani',
     author_email='usmanisufiyaan@gmail.com',
     description='A simple tool to download GitHub repositories or folders.',
-    url='https://github.com/yourusername/github_downloader',
+    url='https://github.com/sufiyaanusmani/github-repo-downloader',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
